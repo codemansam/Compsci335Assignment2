7169e233635bd96625e7187ffad2d2d074271afc
Sam Alexander <sam@stacked.co.nz>
2021-08-31 19:14:22 +1200

    Added DbWebApiRepo and Interface to Data folder.

---
+++ Compsci335Assignment2/Data/DbWebApiRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Compsci335Assignment2.Models;
+
+namespace Compsci335Assignment2.Data
+{
+    public class DbWebApiRepo : IWebApiRepo
+    {
+        private readonly WebAPIDBContext _dbContext;
+
+        public DbWebApiRepo(WebAPIDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public User Register(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

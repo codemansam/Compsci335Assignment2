1207a07194ae73b9e0b003adf4cefc0886d619c0
Sam Alexander <sam@stacked.co.nz>
2021-09-01 21:45:26 +1200

    Started work on second endpoint.  Not working, sleep time

---
+++ Compsci335Assignment2/Controllers/GetVersionAController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Compsci335Assignment2.Data;
+
+namespace Compsci335Assignment2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetVersionAController : ControllerBase
+    {
+        private readonly IWebApiRepo _webApiRepo;
+
+        public GetVersionAController(IWebApiRepo webApiRepo)
+        {
+            _webApiRepo = webApiRepo;
+        }
+
+        [HttpPost]
+        public IActionResult ValidLogin(UserName, Password)
+        {
+            return Ok();
+
+        }
+
+    }
+}
---
+++ Compsci335Assignment2/Data/IauthRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Compsci335Assignment2.Data
+{
+    public interface IauthRepo
+    {
+        public bool ValidLogin(string userName, string password);
+    }
+}
